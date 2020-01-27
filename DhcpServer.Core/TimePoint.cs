7c6eb400bd5f4697cbd865a2ac37042f87cab54a
Brian Rogers <bobbymcr@gmail.com>
2020-01-26 18:44:14 -0800

    TimePoint - ElapsedOneMillisecond

---
+++ DhcpServer.Core/TimePoint.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="TimePoint.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace DhcpServer
+{
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Represents an instant in time.
+    /// </summary>
+    public readonly struct TimePoint
+    {
+        private static readonly double TimerTicksPerSecond = Stopwatch.Frequency;
+        private static readonly long SecondsPerTick = 10000000;
+
+        private readonly long timerTicks;
+
+        private TimePoint(long timerTicks)
+        {
+            this.timerTicks = timerTicks;
+        }
+
+        /// <summary>
+        /// Finds the difference between two time points.
+        /// </summary>
+        /// <param name="x">The minuend.</param>
+        /// <param name="y">The subtrahend.</param>
+        /// <returns>The difference as a <see cref="TimeSpan"/>.</returns>
+        public static TimeSpan operator -(TimePoint x, TimePoint y)
+        {
+            long deltaTimerTicks = x.timerTicks - y.timerTicks;
+            double deltaTicks = SecondsPerTick * deltaTimerTicks / TimerTicksPerSecond;
+            return new TimeSpan((long)deltaTicks);
+        }
+
+        /// <summary>
+        /// Creates a time point at the current time.
+        /// </summary>
+        /// <returns>A new time point.</returns>
+        public static TimePoint Now() => new TimePoint(Stopwatch.GetTimestamp());
+
+        /// <summary>
+        /// Gets the time elapsed since this time point.
+        /// </summary>
+        /// <returns>The elapsed time.</returns>
+        public TimeSpan Elapsed() => Now() - this;
+    }
+}
---
+++ DhcpServer.Test/TimePointTest.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="TimePointTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace DhcpServer.Test
+{
+    using System;
+    using System.Threading;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class TimePointTest
+    {
+        [TestMethod]
+        public void ElapsedOneMillisecond()
+        {
+            TimePoint start = TimePoint.Now();
+
+            Thread.Sleep(1);
+            TimeSpan elapsed = start.Elapsed();
+
+            elapsed.Should().BeGreaterOrEqualTo(TimeSpan.FromMilliseconds(1.0d));
+        }
+    }
+}
